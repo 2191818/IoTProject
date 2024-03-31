425092596468058897ae7cb76dcf685e07cb7288
hqara <hibba_qaraman@hotmail.com>
2024-03-31 18:41:33 -0400

    can display temperature and humidity readings in IoT dashboard

--- Phase2/app.py
+++ Phase2/app.py
@@ -1,20 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import RPi.GPIO as GPIO
 from time import sleep
+import Freenove_DHT as DHT
 
 app = Flask(__name__)
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
+if GPIO.getmode() is None:
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
 
 LED = 27
-#BUTTON = 12
-
 GPIO.setup(LED, GPIO.OUT)
-#GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
 light_on = False
-temperature_reading = 25  # Example temperature reading
+
+DHTPin = 17
 
 def toggle_light():
     global light_on
@@ -24,21 +23,31 @@ def toggle_light():
     else:
         GPIO.output(LED, GPIO.LOW)
         light_on = False
+            
+def read_dht_sensor():
+    dht = DHT.DHT(DHTPin)
+    counts = 0
+    for i in range(0, 15):
+        chk = dht.readDHT11()
+        if chk is dht.DHTLIB_OK:
+            return dht.humidity, dht.temperature
+        sleep(0.1)
+    return None, None
 
 @app.route('/')
 def index():
-    return render_template('index.html', light_status=light_on, temperature=temperature_reading)
+    humidity, temperature = read_dht_sensor()
+    return render_template('index.html', light_status=light_on, temperature=temperature, humidity=humidity)
 
 @app.route('/toggle')
 def toggle():
     toggle_light()
     return 'OK'
 
-#def button_callback(channel):
-#   toggle_light()
-#   app.logger.info("Button pressed, light status changed.")
-
-#GPIO.add_event_detect(BUTTON, GPIO.FALLING, callback=button_callback, bouncetime=300)
+@app.route('/sensor_data')
+def sensor_data():
+    humidity, temperature = read_dht_sensor()
+    return jsonify({'temperature': temperature, 'humidity': humidity})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
